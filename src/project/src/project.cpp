64e8328307b5dcf19afee6694e9d9af25e30b9ac
FilippoBetello <betello.1835108@studenti.uniroma1.it>
2022-01-21 17:22:28 +0100

    First step of laser subscriber

--- src/project/src/project.cpp
+++ src/project/src/project.cpp
@@ -5,9 +5,9 @@
 #include "laser_geometry/laser_geometry.h"
 #include "geometry_msgs/Twist.h"
 #include "message_filters/subscriber.h" 
+#include "tf/message_filter.h"
 
-
-float vel_x = 0, vel_y = 0;
+float vel_x = 0, vel_y = 0, vel_obstacle_x = 0, vel_obstacle_y = 0;
 
 void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg){
     vel_x = msg->linear.x;
@@ -16,8 +16,23 @@ void cmd_vel_callback(const geometry_msgs::Twist::ConstPtr& msg){
     std::cout << vel_y << " Velocity y" << std::endl;
 }
 
-void laser_cmd_vel_callback(const sensor_msgs::LaserScan::ConstPtr& msg){
-  ROS_INFO("Laser_scan: [%s]", msg->header.frame_id.c_str());
+void laser_cmd_vel_callback(const sensor_msgs::LaserScan::ConstPtr& scan){
+
+  laser_geometry::LaserProjection projector;
+  tf::StampedTransform obstacle;
+  tf::TransformListener listener;
+  sensor_msgs::PointCloud cloud;
+  try{
+    // Transform laser scan in odom frame using the tf listener
+    projector.transformLaserScanToPointCloud("base_laser_link",*scan, cloud,listener);
+    listener.waitForTransform("base_footprint", "base_laser_link", ros::Time(0), ros::Duration(10,0));
+    listener.lookupTransform("base_footprint", "base_laser_link", ros::Time(0), obstacle);
+    std::cout << "funziona daje" << std::endl;
+  }
+  catch(tf::TransformException &ex) {
+    ROS_ERROR("%s", ex.what());
+    return;
+  }
 }
 
 int main(int argc, char **argv){
